188e8bb18f3f1559e08d77ddea00bc0e90f82dda
Faizuddin Mohammed <115614604+F21Z@users.noreply.github.com>
2026-04-01 17:59:59 +0000

    set homepage for github pages

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Mohammed Faizuddin",
+  title: "CS UnderGrad | AI Enthusiast",
+  email: "faizuddinrm@gmail.com",
+  gitHub: "https://github.com/F21Z",
+  instagram: "https://www.instagram.com/faizuddinrm/",
+  linkedIn: "https://www.linkedin.com/in/faizuddinrm/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
